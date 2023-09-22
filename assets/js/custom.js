0f210fd7fa067edbdb9a13e004f7e8c47f2d4bd5
LPedroo <plazabr3@gmail.com>
2023-09-22 13:57:24 -0300

    postando projeto

---
+++ assets/js/custom.js
@@ -0,0 +1,58 @@
+//Efeito de rolagem suave à página
+
+jQuery(function ($) {
+  $("a[href*=#]").click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var $target = $(this.hash);
+      $target =
+        ($target.length && $target) || $("[name=" + this.hash.slice(1) + "]");
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $("html,body").animate({ scrollTop: targetOffset }, 800); //set scroll speed here
+        return false;
+      }
+    }
+  });
+
+  window.scrollReveal = new scrollReveal();
+});
+
+//Efeito de digitação na frase
+
+var string = "Seja Bem Vindo ao meu Portfólio!";
+var array = string.split("");
+var timer;
+
+function frameLooper() {
+  if (array.length > 0) {
+    document.getElementById("textEffect").textContent += array.shift();
+  } else {
+    clearTimeout(timer);
+  }
+  loopTimer = setTimeout("frameLooper()", 70);
+}
+frameLooper();
+
+// function remove() {
+//   const menu = document.querySelector(".in");
+//   const item = document.querySelector("#menu");
+
+//   if (menu) {
+//     remove;
+//   } else {
+//     item.classList.add("in");
+//   }
+// }
+
+// menu.addEventListener("click", remove);
+// menu.addEventListener("click", () => {
+//   if (menu) {
+//     item.classList.remove("in");
+//   } else {
+//     item.classList.add("in");
+//   }
+// });
